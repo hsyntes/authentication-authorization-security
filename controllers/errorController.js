10badb179368c57a256dbd214254033a5ef51fa0
hsyntes <hsyntes@gmail.com>
2023-07-07 23:16:18 +0300

    fix mongoose unique error logic in errorController

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,7 +1,10 @@
 const ErrorProvider = require("../classes/ErrorProvider");
 
+const sendError = (statusCode, status, message) =>
+  new ErrorProvider(statusCode, status, message);
+
 // * Modifying the error object according to the the name of error
-const validatorError = (err) => {
+const valdationError = (err) => {
   let messages = err.message.split(",");
 
   const message = messages
@@ -9,7 +12,8 @@ const validatorError = (err) => {
     .join("")
     .trim();
 
-  return new ErrorProvider(403, "fail", message);
+  // return new ErrorProvider(403, "fail", message);
+  return sendError(403, "fail", message);
 };
 
 const uniqueError = (err) => {
@@ -17,21 +21,25 @@ const uniqueError = (err) => {
     err.keyPattern.hasOwnProperty("username") ||
     err.keyPattern.hasOwnProperty("email")
   )
-    return new ErrorProvider(409, "fail", "This user is already in use.");
+    // return new ErrorProvider(409, "fail", "This user is already in use.");
+    return sendError(409, "fail", "This user is already in use.");
 
-  return new ErrorProvider(401, "fail", err.message);
+  // return new ErrorProvider(401, "fail", err.message);
+  return sendError(401, "fail", err.message);
 };
 
 const jsonWebTokenError = () =>
-  new ErrorProvider(404, "fail", "Authentication failed.");
+  // new ErrorProvider(404, "fail", "Authentication failed.");
+  sendError(404, "fail", "Authentication failed.");
 
 const tokenExpiredError = () =>
-  new ErrorProvider(401, "fail", "Authentication has expired. Log in again.");
+  // new ErrorProvider(401, "fail", "Authentication has expired. Please log in again.");
+  sendError(401, "fail", "Authentication has expired. Please log in again.");
 
 // * Export the new error object to handle error globally
 module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === "production") {
-    if (err.name === "ValidationError") err = validatorError(err);
+    if (err.name === "ValidationError") err = valdationError(err);
     if (err.code === 11000) err = uniqueError(err);
     if (err.name === "JsonWebTokenError") err = jsonWebTokenError();
     if (err.name === "TokenExpiredError") err = tokenExpiredError();
